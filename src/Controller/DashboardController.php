2778b79aed67c423c9d9fa328e042c1c3e15130f
Dikoyens <168093326+Dikoyens@users.noreply.github.com>
2025-10-15 13:00:15 +0800

    nausab

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PartRepository;
+use App\Repository\RepairRecordRepository;
+use App\Repository\DeliveryRepository;
+use App\Repository\CategoryRepository;
 
 final class DashboardController extends AbstractController
 {
@@ -13,6 +17,38 @@ final class DashboardController extends AbstractController
     {
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
+        ]);
+    }
+
+    #[Route('/parts', name: 'app_part_index', methods: ['GET'])]
+    public function partIndex(PartRepository $partRepository): Response
+    {
+        return $this->render('part/index.html.twig', [
+            'parts' => $partRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/repairs', name: 'app_repair_record_index', methods: ['GET'])]
+    public function repairRecordIndex(RepairRecordRepository $repairRecordRepository): Response
+    {
+        return $this->render('repair_record/index.html.twig', [
+            'repair_records' => $repairRecordRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/deliveries', name: 'app_delivery_index', methods: ['GET'])]
+    public function deliveryIndex(DeliveryRepository $deliveryRepository): Response
+    {
+        return $this->render('delivery/index.html.twig', [
+            'deliveries' => $deliveryRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/categories', name: 'app_category_index', methods: ['GET'])]
+    public function categoryIndex(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 }
